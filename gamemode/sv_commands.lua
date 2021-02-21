cf97abba957089706081bb7987aecb00e802472e
Tpan496 <tianleip@andrew.cmu.edu>
2021-02-21 16:13:52 -0500

    add console variants of chat commands

--- gamemode/sv_commands.lua
+++ gamemode/sv_commands.lua
@@ -6,6 +6,7 @@ util.AddNetworkString("Horde_ToggleItemConfig")
 util.AddNetworkString("Horde_ToggleEnemyConfig")
 util.AddNetworkString("Horde_ToggleClassConfig")
 util.AddNetworkString("Horde_RenderCenterText")
+util.AddNetworkString("Horde_Console_Commands")
 
 function BroadcastMessage(msg, delay)
     for _, ply in pairs(player.GetAll()) do
@@ -22,6 +23,143 @@ hook.Add("PlayerInitialSpawn", "Horde_SyncItems", function (ply)
     net.Send(ply)
 end)
 
+function Start(ply)
+    if ply:IsAdmin() then
+        if not HORDE.start_game then
+            HORDE.start_game = true
+            BroadcastMessage("Horde gamemode initiated!")
+        else
+            ply:PrintMessage(HUD_PRINTTALK, "Game has already started!")
+        end
+    else
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You do not have access to this command.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+end
+
+function Ready(ply)
+    if not ply:Alive() then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You can't get ready when you are dead!")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    if HORDE.start_game then return end
+    HORDE.player_ready[ply:SteamID()] = true
+    local ready_count = 0
+    local total_player = 0
+    for _, ply in pairs(player.GetAll()) do
+        if HORDE.player_ready[ply:SteamID()] then
+            ready_count = ready_count + 1
+        end
+        total_player = total_player + 1
+    end
+   
+    if total_player == ready_count then
+        HORDE.start_game = true
+    end
+    BroadcastMessage("Players Ready: " .. tostring(ready_count) .. "/" .. tostring(total_player))
+end
+
+function End(ply)
+    if not ply:IsAdmin() then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You do not have access to this command.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    HORDE.start_game = false
+    HORDE.player_ready = {}
+    ply:PrintMessage(HUD_PRINTTALK, "Stopping game...")
+end
+
+function Shop(ply)
+    if not HORDE.enable_shop then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("Shop has been disabled.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+    end
+    if not ply:Alive() then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You can't buy when you are dead!")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    if HORDE.current_break_time <= 0 then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You cannot shop after a wave has started.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    net.Start("Horde_ToggleShop")
+    net.Send(ply)
+end
+
+function ItemConfig(ply)
+    if HORDE.start_game then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You cannot open config after a wave has started.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    if ply:IsSuperAdmin() then
+        net.Start("Horde_ToggleItemConfig")
+        net.Send(ply)
+    else
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You do not have access to this command.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+    end
+end
+
+function EnemyConfig(ply)
+    if HORDE.start_game then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You cannot open config after a wave has started.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    if ply:IsSuperAdmin() then
+        net.Start("Horde_ToggleEnemyConfig")
+        net.Send(ply)
+    else
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You do not have access to this command.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+    end
+end
+
+function ClassConfig(ply)
+    if HORDE.start_game then
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You cannot open config after a wave has started.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+        return
+    end
+    if ply:IsSuperAdmin() then
+        net.Start("Horde_ToggleClassConfig")
+        net.Send(ply)
+    else
+        net.Start("Horde_LegacyNotification")
+        net.WriteString("You do not have access to this command.")
+        net.WriteInt(1,2)
+        net.Send(ply)
+    end
+end
+
 hook.Add("PlayerSay", "Horde_Commands", function(ply, input, public)
     local text = string.lower(input) -- Make the chat message entirely lowercase
     if text == "!help" then
@@ -31,133 +169,57 @@ hook.Add("PlayerSay", "Horde_Commands", function(ply, input, public)
         ply:PrintMessage(HUD_PRINTTALK, "'!end' - End the game (Admin only)")
     
     elseif text == "!start" then
-        if ply:IsAdmin() then
-            if not HORDE.start_game then
-                HORDE.start_game = true
-                BroadcastMessage("Horde gamemode initiated!")
-            else
-                ply:PrintMessage(HUD_PRINTTALK, "Game has already started!")
-            end
-        else
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You do not have access to this command.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
+        Start(ply)
 
     elseif text == "!ready" then
-        if not ply:Alive() then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You can't get ready when you are dead!")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        if HORDE.start_game then return end
-        HORDE.player_ready[ply:SteamID()] = true
-        local ready_count = 0
-        local total_player = 0
-        for _, ply in pairs(player.GetAll()) do
-            if HORDE.player_ready[ply:SteamID()] then
-                ready_count = ready_count + 1
-            end
-            total_player = total_player + 1
-        end
-       
-        if total_player == ready_count then
-            HORDE.start_game = true
-        end
-        BroadcastMessage("Players Ready: " .. tostring(ready_count) .. "/" .. tostring(total_player))
+        Ready(ply)
     
     elseif text == "!end" then
-        if not ply:IsAdmin() then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You do not have access to this command.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        HORDE.start_game = false
-        HORDE.player_ready = {}
-        ply:PrintMessage(HUD_PRINTTALK, "Stopping game...")
+        End(ply)
 
     elseif text == "!shop" then
-        if not HORDE.enable_shop then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("Shop has been disabled.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-        end
-        if not ply:Alive() then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You can't buy when you are dead!")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        if HORDE.current_break_time <= 0 then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You cannot shop after a wave has started.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        net.Start("Horde_ToggleShop")
-        net.Send(ply)
+        Shop(ply)
+
     elseif text == "!itemconfig" then
-        if HORDE.start_game then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You cannot open config after a wave has started.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        if ply:IsSuperAdmin() then
-            net.Start("Horde_ToggleItemConfig")
-            net.Send(ply)
-        else
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You do not have access to this command.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-        end
+        ItemConfig(ply)
+        
     elseif text == "!enemyconfig" then
-        if HORDE.start_game then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You cannot open config after a wave has started.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        if ply:IsSuperAdmin() then
-            net.Start("Horde_ToggleEnemyConfig")
-            net.Send(ply)
-        else
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You do not have access to this command.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-        end
+        EnemyConfig(ply)
+        
     elseif text == "!classconfig" then
-        if HORDE.start_game then
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You cannot open config after a wave has started.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-            return
-        end
-        if ply:IsSuperAdmin() then
-            net.Start("Horde_ToggleClassConfig")
-            net.Send(ply)
-        else
-            net.Start("Horde_LegacyNotification")
-            net.WriteString("You do not have access to this command.")
-            net.WriteInt(1,2)
-            net.Send(ply)
-        end
+        ClassConfig(ply)
+        
     end
     return input
+end)
+
+-- Console variants
+concommand.Add("horde_start", function (ply, cmd, args)
+    Start(ply)
+end)
+
+concommand.Add("horde_ready", function (ply, cmd, args)
+    Ready(ply)
+end)
+
+concommand.Add("horde_end", function (ply, cmd, args)
+    End(ply)
+end)
+
+concommand.Add("horde_shop", function (ply, cmd, args)
+    Shop(ply)
+end)
+
+concommand.Add("horde_item_config", function (ply, cmd, args)
+    ItemConfig(ply)
+end)
+
+concommand.Add("horde_enemy_config", function (ply, cmd, args)
+    EnemyConfig(ply)
+end)
+
+concommand.Add("horde_class_config", function (ply, cmd, args)
+    ClassConfig(ply)
 end)
 
 hook.Add("PlayerInitialSpawn", "Horde_SpawnMessage", function(ply)
