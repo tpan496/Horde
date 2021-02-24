7619dd5acc4d494803583daeb3a83d7b07b6f273
TheOnly8Z <8888888zombies@gmail.com>
2021-02-24 15:34:16 +0800

    Add nice dosh sounds from KF2

--- entities/entities/horde_money/init.lua
+++ entities/entities/horde_money/init.lua
@@ -6,11 +6,12 @@ ENT.CleanupPriority = 2
 function ENT:Initialize()
     self:SetColor(IsMounted("cstrike") and Color(255, 255, 255) or Color(0, 255, 0))
     self:SetModel(IsMounted("cstrike") and "models/props/cs_assault/Money.mdl" or "models/items/flare.mdl")
-    self:SetModelScale(2)
-    self:PhysicsInitBox(Vector(-4, -4, -1), Vector(4, 4, 1))
+    self:PhysicsInit(SOLID_VPHYSICS)
+    --self:PhysicsInitBox(Vector(-4, -4, -1), Vector(4, 4, 1))
     self:SetSolid(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetCollisionGroup(COLLISION_GROUP_WEAPON)
+    self:SetModelScale(2)
     self:SetTrigger(true)
     self:UseTriggerBounds(true, 4)
 
@@ -23,7 +24,7 @@ function ENT:StartTouch(entity)
         self.Removing = true
         entity:AddMoney(50)
         entity:SyncEconomy()
-        entity:EmitSound("physics/metal/metal_computer_impact_soft2.wav", 70, 200, 0.1)
+        entity:EmitSound("horde/dosh_" .. math.random(1, 4) .. ".ogg", 70, 100, 0.25)
         self:Remove()
     end
 end
