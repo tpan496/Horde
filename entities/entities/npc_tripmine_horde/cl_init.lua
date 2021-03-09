2ba0a4d3461d3d63b579208a03cf9fc591f51c0e
Ulti <49292935+Ultimeit@users.noreply.github.com>
2021-03-09 15:57:55 +0000

    Requested changes
    Also buffed damage, because incendiary grenade exist, while they both have same price per use(100$ per 1 ammo). Not it's 2.4x of the RPG rocket(10$ ammo btw) direct hit damage. I wouldn't suggest using that without demolition class >.>

--- entities/entities/npc_tripmine_horde/cl_init.lua
+++ entities/entities/npc_tripmine_horde/cl_init.lua
@@ -3,16 +3,13 @@ killicon.AddAlias("npc_tripmine_horde", "npc_tripmine")
 
 local lmat = Material("trails/laser")
 local color_armed, color_enemy = Color(0, 200, 200), Color(255, 0, 0)
-function ENT:Draw()
-	self:DrawModel()
-end
 
 function ENT:DrawLaser()
-	if not self.Armed or not self.TraceEnd then return end
-	render.SetMaterial(lmat)
-	if self:GetTriggered() then
-		render.DrawBeam(self.TraceStart, self.TraceEnd, 7, 1, 1, color_enemy)
-	else
-		render.DrawBeam(self.TraceStart, self.TraceEnd, 7, 1, 1, color_armed)
-	end
+    if not self.Armed or not self.TraceEnd then return end
+    render.SetMaterial(lmat)
+    if self:GetTriggered() then
+        render.DrawBeam(self.TraceStart, self.TraceEnd, 7, 1, 1, color_enemy)
+    else
+        render.DrawBeam(self.TraceStart, self.TraceEnd, 7, 1, 1, color_armed)
+    end
 end
