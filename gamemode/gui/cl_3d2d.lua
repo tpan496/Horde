3a02c49059ff4d5c01ecbbf6e549554d77f31618
Tpan496 <tianleip@andrew.cmu.edu>
2021-03-16 00:20:24 -0400

    spaces...

--- gamemode/gui/cl_3d2d.lua
+++ gamemode/gui/cl_3d2d.lua
@@ -1,61 +1,59 @@
 surface.CreateFont("Icon", { font = "arial", size = 64 })
 
 local function Render(bdepth, bskybox)
-	if bskybox then return end
+    if bskybox then return end
 
-	for _, ply in pairs(player.GetAll()) do
-		if not IsValid(ply) or (ply == LocalPlayer()) then
-			goto cont
-		end
+    for _, ply in pairs(player.GetAll()) do
+        if not IsValid(ply) or (ply == LocalPlayer()) then
+            goto cont
+        end
 
         local class = "Demolition"
         if ply:GetHordeClass() then class = ply:GetHordeClass().name end
 
-		local attachment_id = ply:LookupAttachment("anim_attachment_head") or 0 -- Can't take any chances with nils here
-		local attachment = ply:GetAttachment(attachment_id)
+        local attachment_id = ply:LookupAttachment("anim_attachment_head") or 0 -- Can't take any chances with nils here
+        local attachment = ply:GetAttachment(attachment_id)
 
-		local base_pos = Vector()
+        local base_pos = Vector()
 
-		if attachment and attachment.Pos then -- Would've been too ugly with a ternary
-			base_pos = attachment.Pos
-		else
-			base_pos = (ply:LocalToWorld(ply:OBBCenter()) + ply:GetUp() * 24)
-		end
+        if attachment and attachment.Pos then -- Would've been too ugly with a ternary
+            base_pos = attachment.Pos
+        else
+            base_pos = (ply:LocalToWorld(ply:OBBCenter()) + ply:GetUp() * 24)
+        end
 
-		local render_pos = base_pos + ply:GetUp() * 15
+        local render_pos = base_pos + ply:GetUp() * 15
 
-		-- Distance fading
-		local render_mindist = 500
-		local render_maxdist = render_mindist + 80
+        -- Distance fading
+        local render_mindist = 500
+        local render_maxdist = render_mindist + 80
 
-		local dist = (render_pos - EyePos()):Length()
-		local dist_clamped = math.Clamp(dist, render_mindist, render_maxdist)
-		local dist_alpha = math.Remap(dist_clamped, render_mindist, render_maxdist, 200, 0)
+        local dist = (render_pos - EyePos()):Length()
+        local dist_clamped = math.Clamp(dist, render_mindist, render_maxdist)
+        local dist_alpha = math.Remap(dist_clamped, render_mindist, render_maxdist, 200, 0)
 
-		if dist_alpha == 0 then goto cont end -- Nothing to render
+        if dist_alpha == 0 then goto cont end -- Nothing to render
 
-		-- Colors
-		local fade_white = Color(255, 255, 255, dist_alpha)
+        -- Colors
+        local fade_white = Color(255, 255, 255, dist_alpha)
 
-		-- Render ang
-		local render_ang = EyeAngles()
-		render_ang:RotateAroundAxis(render_ang:Right(),90)
-		render_ang:RotateAroundAxis(-render_ang:Up(),90)
+        -- Render ang
+        local render_ang = EyeAngles()
+        render_ang:RotateAroundAxis(render_ang:Right(),90)
+        render_ang:RotateAroundAxis(-render_ang:Up(),90)
 
         local mat = Material("materials/" .. class .. ".png", "mips smooth")
         local len = string.len(class) * 6
 
-		cam.Start3D2D(render_pos, render_ang, 0.1)
-			--render.PushFilterMag(TEXFILTER.POINT) -- Make it pixelated
-			surface.SetMaterial(mat)
-			surface.SetDrawColor(fade_white)
-			surface.DrawTexturedRect(- 64 - len - 8, -64 / 2, 64, 64)
+        cam.Start3D2D(render_pos, render_ang, 0.1)
+            surface.SetMaterial(mat)
+            surface.SetDrawColor(fade_white)
+            surface.DrawTexturedRect(- 64 - len - 8, -64 / 2, 64, 64)
             draw.SimpleText(class, "Icon", len + 8, 0, fade_white, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-			--render.PopFilterMag()
 
-		cam.End3D2D()
-		::cont::
-	end
+        cam.End3D2D()
+        ::cont::
+    end
 
 end
 
