282cd90e8ae6958f229ab6137897f48c5cf00fa9
Tpan496 <tianleip@andrew.cmu.edu>
2021-03-29 21:12:44 -0400

    hotfix

--- gamemode/perks/demolition_fragmentation.lua
+++ gamemode/perks/demolition_fragmentation.lua
@@ -1,11 +1,11 @@
 PERK.PrintName = "Fragmentation"
-PERK.Description = "25% increased damage against enemies on full health."
+PERK.Description = "25% increased Blast damage against enemies on full health."
 PERK.Icon = "materials/perks/fragmentation.png"
 
 PERK.Hooks = {}
-PERK.Hooks.Horde_OnPlayerDamage = function (ply, npc, bonus, hitgroup)
+PERK.Hooks.Horde_OnPlayerDamage = function (ply, npc, bonus, hitgroup, dmginfo)
     if not ply:Horde_GetPerk("demolition_fragmentation")  then return end
-    if npc:Health() == npc:GetMaxHealth() then
+    if npc:Health() == npc:GetMaxHealth() and dmginfo:GetDamageType() == DMG_BLAST then
         bonus.increase = bonus.increase + 0.25
     end
 end
