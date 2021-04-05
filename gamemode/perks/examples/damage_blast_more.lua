c3d686719b327279d0d2d65bc5dfbb1fd2c274ad
Tpan496 <tianleip@andrew.cmu.edu>
2021-04-04 21:06:56 -0400

    fix dumbass map vote

--- gamemode/perks/examples/damage_blast_more.lua
+++ gamemode/perks/examples/damage_blast_more.lua
@@ -6,7 +6,7 @@ PERK.Hooks.OnPlayerDamage = function (ply, npc, bonus, hitgroup, dmginfo)
     local attacker = dmginfo:GetAttacker()
     if attacker:IsPlayer() and (dmginfo:IsDamageType(DMG_BLAST) or dmginfo:IsExplosionDamage())
             and attacker:Horde_GetPerk("damage_blast_more") then
-        bonus.increase = bonus.increase * 1.5
+        bonus.more = bonus.more * 1.5
     end
 end
 
