267a64d84793effad0a0503e706a8b33cd71a43d
Tpan496 <tianleip@andrew.cmu.edu>
2021-04-07 16:32:46 -0400

    complete english perks, for now

--- gamemode/languages/english.lua
+++ gamemode/languages/english.lua
@@ -74,11 +74,42 @@ LANGUAGE.Class_Ghost = "Ghost"
 LANGUAGE.Class_Engineer = "Engineer"
 LANGUAGE.Class_Berserker = "Berserker"
 
+
+-- Default Class Extra Descriptions
+LANGUAGE["Class_Description_Survivor"] = [[
+Have access to all weapons except for exclusive and special weapons.
+Limited access to attachments.]]
+
+LANGUAGE["Class_Description_Assault"] = [[
+Have full access to Assault Rifles."]]
+
+LANGUAGE["Class_Description_Heavy"] = [[
+Have full access to Machine Guns and high weight weapons.]]
+
+LANGUAGE["Class_Description_Medic"] = [[
+Have acesss to most light weapons and medic grenades.]]
+
+LANGUAGE["Class_Description_Demolition"] = [[
+Have full access to explosive weapons.]]
+
+LANGUAGE["Class_Description_Ghost"] = [[
+Have access to sniper rifles and selected light weapons.
+Have access to suppressors and sniper scopes.]]
+
+LANGUAGE["Class_Description_Engineer"] = [[
+Have access to special weapons and equipment.]]
+
+LANGUAGE["Class_Description_Berserker"] = [[
+Only has access to melee weapons."]]
+
+
+-- Default Base Perks
 LANGUAGE.Perk_survivor_base = [[
 The Survivor class can be played into any class to fill in missing roles for the team.
 Complexity: EASY
 
 No bonus.]]
+
 LANGUAGE.Perk_assault_base = [[
 The Assault class is an all-purpose fighter with high mobility and a focus on Adrenaline stacks.
 Complexity: EASY
@@ -86,23 +117,27 @@ Complexity: EASY
 20% increased movement speed.
 Gain Adrenaline when you kill an enemy.
 Adrenaline increases damage and speed by 6%.]]
+
 LANGUAGE.Perk_heavy_base = [[
 The Heavy class is a tank class that provides strong suppression firepower.
 Complexity: EASY
 
 Regenerate 1 armor per second.
 Regenerate up to 25 armor.]]
+
 LANGUAGE.Perk_medic_base = [[
 The Medic class is a durable support class that focuses on healing and buffing teammates.
 Complexity: MEDIUM
 
 Regenerate 2% health per second.]]
+
 LANGUAGE.Perk_demolition_base = [[
 The Demolition class is a crowd-control class that can also provide high single target damage.
 Complexity: MEDIUM
 
 85% increased Blast damage resistance.
 Regenerate 1 frag grenade every 30 seconds, if you do not have one.]]
+
 LANGUAGE.Perk_ghost_base = [[
 The Ghost class is focused on taking down boss enemies using Camoflague.
 Complexity: HIGH
@@ -110,15 +145,256 @@ Complexity: HIGH
 25% more headshot damage.
 Crouch to activate Camoflague, granting 15% evasion.
 Attacking, Running or Jumping REMOVES Camoflague.]]
+
 LANGUAGE.Perk_engineer_base = [[
 The Engineer class is a minion-centered class that deals damage through minions.
 Complexity: MEDIUM
 
 25% increased minion health and damage.
 Turrets have 500 base health and deals 18 base damage.]]
+
 LANGUAGE.Perk_berserker_base = [[
 The Berserker class is a melee-centered class that can be played both offensively and defensively.
 Complexity: HIGH
 
 20% increased Global damage resistance.
 25% increased Poison damage resistance.]]
+
+
+-- Default Perks: Survivor
+LANGUAGE["Perk_Title_Survivor_Tier_1"] = [[Survival]]
+LANGUAGE["Perk_Title_Survivor_Tier_2"] = [[Improvise]]
+LANGUAGE["Perk_Title_Survivor_Tier_3"] = [[Inspired Learning]]
+
+-- Default Perks: Assault
+LANGUAGE["Perk_Title_Assault_Tier_1"] = [[Maneuverability]]
+LANGUAGE["Perk_Title_assault_ambush"] = [[Ambush]]
+LANGUAGE["Perk_assault_ambush"] = [[
+15% increased headshot damage.]]
+
+LANGUAGE["Perk_Title_assault_charge"] = [[Charge]]
+LANGUAGE["Perk_assault_charge"] = [[
+Adds 2 maximum Adrenaline stacks.
+(Each Adrenaline stack increases damage and speed by 6%.)]]
+
+LANGUAGE["Perk_Title_Assault_Tier_2"] = [[Adaptability]]
+LANGUAGE["Perk_Title_assault_drain"] = [[Drain]]
+LANGUAGE["Perk_assault_drain"] = [[
+15% increased headshot damage when on full health.
+Recover 2% health for each enemy you killed.]]
+
+LANGUAGE["Perk_Title_assault_overclock"] = [[Overclock]]
+LANGUAGE["Perk_assault_overclock"] = [[
+Adrenaline duration increased by 50%.
+Adds 2 maximum Adrenaline stacks.]]
+
+LANGUAGE["Perk_Title_Assault_Tier_3"] = [[Conditioning]]
+LANGUAGE["Perk_Title_assault_heightened_reflex"] = [[Heightened Reflex]]
+LANGUAGE["Perk_assault_heightened_reflex"] = [[
+20% increased headshot damage.
+20% increased global damage resistance on full health.]]
+
+LANGUAGE["Perk_Title_assault_cardiac_overload"] = [[Cardiac Overload]]
+LANGUAGE["Perk_assault_cardiac_overload"] = [[
+25% chance to add 1 Adrenaline stack on headshot.
+Adds 2 maximum Adrenaline stacks.]]
+
+-- Default Perks: Heavy
+LANGUAGE["Perk_Title_Heavy_Tier_1"] = [[Suppression]]
+LANGUAGE["Perk_Title_heavy_sticky_compound"] = [[Sticky Compound]]
+LANGUAGE["Perk_heavy_sticky_compound"] = [[
+Enemies you hit with Ballistic damage are Slowed.
+Slowed enemies have 20% reduced action speed.]]
+
+LANGUAGE["Perk_Title_heavy_crude_casing"] = [[Crude Casing]]
+LANGUAGE["Perk_heavy_crude_casing"] = [[
+Enemies you hit with Ballistic damage are Weakened.
+Weakened enemies deal 10% reduced damage.
+Weakened enemies receive 10% increased damage.]]
+
+LANGUAGE["Perk_Title_Heavy_Tier_2"] = [[Armor Protection]]
+LANGUAGE["Perk_Title_heavy_liquid_armor"] = [[Liquid Armor]]
+LANGUAGE["Perk_heavy_liquid_armor"] = [[
+While you have at least 5 armor:
+  75% increasaed Physical damage resistance.]]
+
+LANGUAGE["Perk_Title_heavy_reactive_armor"] = [[Reactive Armor]]
+LANGUAGE["Perk_heavy_reactive_armor"] = [[
+While you have at least 5 armor:\
+  Immune to Poison, Fire and Blast damage.]]
+
+LANGUAGE["Perk_Title_Heavy_Tier_3"] = [[Technology]]
+LANGUAGE["Perk_Title_heavy_nanomachine"] = [[Nanomachine]]
+LANGUAGE["Perk_heavy_nanomachine"] = [[
+100% increased armor regeneration rate.
+You can now regenerate up to 35 armor.]]
+
+LANGUAGE["Perk_Title_heavy_ballistic_shock"] = [[Ballistic Shock]]
+LANGUAGE["Perk_heavy_ballistic_shock"] = [[
+Debuffs you apply have 100% increased effect.
+Debuffs you apply have 100% increased duration.]]
+
+-- Default Perks: Medic
+LANGUAGE["Perk_Title_Medic_Tier_1"] = [[Medicine]]
+LANGUAGE["Perk_Title_medic_antibiotics"] = [[Antibiotics]]
+LANGUAGE["Perk_medic_antibiotics"] = [[
+Your healing is amplified by 15%.]]
+
+LANGUAGE["Perk_Title_medic_painkillers"] = [[Painkillers]]
+LANGUAGE["Perk_medic_painkillers"] = [[
+You can overheal player's health by 15%.]]
+
+LANGUAGE["Perk_Title_Medic_Tier_2"] = [[Bio-Engineering]]
+LANGUAGE["Perk_Title_medic_berserk"] = [[Berserk]]
+LANGUAGE["Perk_medic_berserk"] = [[
+Players you heal have Berserk for 5 seconds.
+10% increased damage and speed while you have Berserk.]]
+
+LANGUAGE["Perk_Title_medic_fortify"] = [[Fortify]]
+LANGUAGE["Perk_medic_fortify"] = [[
+Players you heal have Fortify for 5 seconds.
+15% less damage taken while you have Fortify.]]
+
+LANGUAGE["Perk_Title_Medic_Tier_3"] = [[Natural Selection]]
+LANGUAGE["Perk_Title_medic_cellular_implosion"] = [[Cellular Implosion]]
+LANGUAGE["Perk_medic_cellular_implosion"] = [[
+Enemies you killed have 20% chance to generate a healing cloud.]]
+
+LANGUAGE["Perk_Title_medic_xcele"] = [[Accelerated Healing Factor]]
+LANGUAGE["Perk_medic_xcele"] = [[
+50% increased health regeneration per second.
+Buffs you apply are 100% more effective.]]
+
+-- Default Perks: Demolition
+LANGUAGE["Perk_Title_Demolition_Tier_1"] = [[Grenade]]
+LANGUAGE["Perk_Title_demolition_frag_impact"] = [[Impact Grenade]]
+LANGUAGE["Perk_demolition_frag_impact"] = [[
+Grenades explode on enemy impact.
+Grenade damage increased by 50%.]]
+
+LANGUAGE["Perk_Title_demolition_frag_cluster"] = [[Cluster Grenade]]
+LANGUAGE["Perk_demolition_frag_cluster"] = [[
+Grenades and SLAMs spawn 3 mini-nades on detonation.
+Mini-nades do 25% less damage.]]
+
+LANGUAGE["Perk_Title_Demolition_Tier_2"] = [[Approach]]
+LANGUAGE["Perk_Title_demolition_fragmentation"] = [[Fragmentation]]
+LANGUAGE["Perk_demolition_fragmentation"] = [[
+25% increased Blast damage against enemies on full health.]]
+
+LANGUAGE["Perk_Title_demolition_napalm"] = [[Napalm]]
+LANGUAGE["Perk_demolition_napalm"] = [[
+25% increased Fire damage.
+75% reduced Fire damage taken.
+]]
+
+LANGUAGE["Perk_Title_Demolition_Tier_3"] = [[Destruction]]
+LANGUAGE["Perk_Title_demolition_pressurized_warhead"] = [[Pressurized Warhead]]
+LANGUAGE["Perk_demolition_pressurized_warhead"] = [[
+25% increased Blast damage against elites.]]
+
+LANGUAGE["Perk_Title_demolition_chain_reaction"] = [[Chain Reaction]]
+LANGUAGE["Perk_demolition_chain_reaction"] = [[
+Enemies you kill have 25% chance to explode,
+dealing 50% of their health as Blast damage.]]
+
+-- Default Perks: Ghost
+LANGUAGE["Perk_Title_Ghost_Tier_1"] = [[Tactics]]
+LANGUAGE["Perk_Title_ghost_headhunter"] = [[Headhunter]]
+LANGUAGE["Perk_ghost_headhunter"] = [[
+Headshot damage stacks up to 5 times.
+Each stack increases 8% headshot damage.]]
+
+LANGUAGE["Perk_Title_ghost_sniper"] = [[Sniper]]
+LANGUAGE["Perk_ghost_sniper"] = [[
+25% increased Ballistic damage while crouching.]]
+
+LANGUAGE["Perk_Title_Ghost_Tier_2"] = [[Reposition]]
+LANGUAGE["Perk_Title_ghost_phase_walk"] = [[Phase Walk]]
+LANGUAGE["Perk_ghost_phase_walk"] = [[
+Running/Jumping does not cancel Camoflague.
+25% increased movement speed when you have Camoflague.]]
+
+LANGUAGE["Perk_Title_ghost_ghost_veil"] = [[Ghost Veil]]
+LANGUAGE["Perk_ghost_ghost_veil"] = [[
+Attacking does not cancel Camoflague.
+100% increased Camoflague activation speed.]]
+
+LANGUAGE["Perk_Title_Ghost_Tier_3"] = [[Disposal]]
+LANGUAGE["Perk_Title_ghost_coup"] = [[Coup De Grace]]
+LANGUAGE["Perk_ghost_coup"] = [[
+Instantly kills enemies with less than 20% health.]]
+
+LANGUAGE["Perk_Title_ghost_decapitate"] = [[Decapitate]]
+LANGUAGE["Perk_ghost_decapitate"] = [[
+When you have Camoflague:
+  25% increased Ballistic damage.
+  50% increased headshot damage against elites.]]
+
+-- Default Perks: Engineer
+LANGUAGE["Perk_Title_Engineer_Tier_1"] = [[Craftsmanship]]
+LANGUAGE["Perk_Title_engineer_tinkerer"] = [[Tinkerer]]
+LANGUAGE["Perk_engineer_tinkerer"] = [[
+Minions have 25% more health on spawn.]]
+
+LANGUAGE["Perk_Title_engineer_manhack"] = [[Manhack]]
+LANGUAGE["Perk_engineer_manhack"] = [[
+Your have Manhacks instead of Turrets.
+Manhack dies on impact and regenerates automatically.
+Manhack deals its health as damage.]]
+
+LANGUAGE["Perk_Title_Engineer_Tier_2"] = [[Archetype]]
+LANGUAGE["Perk_Title_engineer_golem"] = [[Golem]]
+LANGUAGE["Perk_engineer_golem"] = [[
+Minions regenerate 2% health per second.
+Minion attacks have increased knockback.]]
+
+LANGUAGE["Perk_Title_engineer_spectre"] = [[Spectre]]
+LANGUAGE["Perk_engineer_spectre"] = [[
+Minions leech 10% of damage dealt.
+Minions deal 15% more damage.]]
+
+LANGUAGE["Perk_Title_Engineer_Tier_3"] = [[Experimental]]
+LANGUAGE["Perk_Title_engineer_symbiosis"] = [[Symbiosis]]
+LANGUAGE["Perk_engineer_symbiosis"] = [[
+Minions have 25% more health on spawn.
+6% less damage taken for each minion alive.]]
+
+LANGUAGE["Perk_Title_engineer_kamikaze"] = [[Kamikaze]]
+LANGUAGE["Perk_engineer_kamikaze"] = [[
+Minions deal 200% of their max health as Blast damage on death.]]
+
+-- Default Perks: Berserker
+LANGUAGE["Perk_Title_Berserker_Tier_1"] = [[Fundamentals]]
+LANGUAGE["Perk_Title_berserker_breathing_technique"] = [[Breathing Technique]]
+LANGUAGE["Perk_berserker_breathing_technique"] = [[
+Regenerate 2% health per second.
+Immune to Poison damage.]]
+
+LANGUAGE["Perk_Title_berserker_bloodlust"] = [[Bloodlust]]
+LANGUAGE["Perk_berserker_bloodlust"] = [[
+Leech 10% of melee damage done.
+Leech is capped at 10 hp per hit.]]
+
+LANGUAGE["Perk_Title_Berserker_Tier_2"] = [[Technique]]
+LANGUAGE["Perk_Title_berserker_bushido"] = [[Bushido]]
+LANGUAGE["Perk_berserker_bushido"] = [[
+25% increased Slashing damage.
+25% increased movement speed.]]
+
+LANGUAGE["Perk_Title_berserker_savagery"] = [[Savagery]]
+LANGUAGE["Perk_berserker_savagery"] = [[
+25% increased Blunt damage.
+25% increased maximum health.]]
+
+LANGUAGE["Perk_Title_Berserker_Tier_3"] = [[Combat Arts]]
+LANGUAGE["Perk_Title_berserker_mindeye"] = [[Mind's Eye]]
+LANGUAGE["Perk_berserker_mindeye"] = [[
+While holding a melee weapon:
+  25% chance to evade damage.
+  Counter with 50% of evaded melee damage.]]
+
+LANGUAGE["Perk_Title_berserker_rip_and_tear"] = [[Rip and Tear]]
+LANGUAGE["Perk_berserker_rip_and_tear"] = [[
+50% more melee damage.
+10% less Global damage resistance.]]
