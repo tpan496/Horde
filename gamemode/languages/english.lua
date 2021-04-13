9686cd4624f56d4fbb9ef7e60267090260588aea
Tpan496 <tianleip@andrew.cmu.edu>
2021-04-13 18:33:13 -0400

    no newline

--- gamemode/languages/english.lua
+++ gamemode/languages/english.lua
@@ -322,8 +322,7 @@ LANGUAGE["Perk_demolition_fragmentation"] = [[
 LANGUAGE["Perk_Title_demolition_napalm"] = [[Napalm]]
 LANGUAGE["Perk_demolition_napalm"] = [[
 {1} increased Fire damage.
-{2} increased Fire damage resistance.
-]]
+{2} increased Fire damage resistance.]]
 
 LANGUAGE["Perk_Title_Demolition_Tier_3"] = [[Destruction]]
 LANGUAGE["Perk_Title_demolition_pressurized_warhead"] = [[Pressurized Warhead]]
