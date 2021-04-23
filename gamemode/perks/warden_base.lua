6257cbbe759d9c5f6977fa0f2cc918b72095410b
Tpan496 <tianleip@andrew.cmu.edu>
2021-04-23 02:49:22 -0400

    fucked up desc

--- gamemode/perks/warden_base.lua
+++ gamemode/perks/warden_base.lua
@@ -1,8 +1,7 @@
 PERK.PrintName = "Warden Base"
-PERK.Description = "The Warden is a support class that uses aura effects and structures to buff teammates.\nComplexity: MEDIUM\n\nPlayers/Minions near you are affected by Warden Aura.\nWarden Aura blocks {1} damage per damage taken.\nWarden Aura has {2} increased effect on you."
+PERK.Description = "The Warden is a support class that uses aura effects and structures to buff teammates.\nComplexity: MEDIUM\n\nPlayers/Minions near you are affected by Warden Aura.\nWarden Aura blocks {1} damage."
 PERK.Params = {
-    [1] = {value = 5},
-    [2] = {value = 0.5, percent = true},
+    [1] = {value = 4}
 }
 
 PERK.Hooks = {}
