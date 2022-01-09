290744b01fbadb81d21307ba150b7ce4b7d84c42
tpan496 <tianleip@andrew.cmu.edu>
2022-01-09 11:04:09 -0500

    remove garbage

--- entities/autorun/vj.lua
+++
@@ -1,4 +0,0 @@
-if (!file.Exists("autorun/vj_base_autorun.lua","LUA")) then return end
-VJ.AddNPC_HUMAN("Father Grigori","npc_vj_horde_grigori",{"weapon_vj_horde_annabelle"}, "Horde")
-
-VJ.AddNPCWeapon("VJ Annabelle", "weapon_vj_horde_annabelle")
