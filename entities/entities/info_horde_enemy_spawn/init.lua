2d71168aef0f6f8ca2a7c2acde632f6d12004f5d
kirillbrest123 <70514395+kirillbrest123@users.noreply.github.com>
2023-04-11 00:02:39 +0300

    Update init.lua

--- entities/entities/info_horde_enemy_spawn/init.lua
+++ entities/entities/info_horde_enemy_spawn/init.lua
@@ -16,12 +16,15 @@ function ENT:AcceptInput(name, activator, caller, arg)
 	name = string.lower(name)
 	if name == "enable" or name == "unhide" then
 		self.Disabled = false
+		HORDE:AddAINode(self:GetPos())
 		return true
 	elseif name == "disable" or name == "hide" then
 		self.Disabled = true
+		HORDE:RemoveAINode(self:GetPos())
 		return true
 	elseif name == "toggle" then
 		self.Disabled = not self.Disabled
+		if self.Disabled then HORDE:RemoveAINode(self:GetPos()) else HORDE:AddAINode(self:GetPos()) end
 		return true
 	end
 end
