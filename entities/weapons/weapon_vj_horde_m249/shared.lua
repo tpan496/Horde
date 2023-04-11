1789efa5aefa7536703585e2144e864de230e12d
tpan496 <tianleip@andrew.cmu.edu>
2023-04-10 21:39:00 -0400

    1.2.0f

--- entities/weapons/weapon_vj_horde_m249/shared.lua
+++ entities/weapons/weapon_vj_horde_m249/shared.lua
@@ -14,9 +14,15 @@ SWEP.SlotPos					= 4 -- Which part of that slot do you want the SWEP to be in? (
 SWEP.UseHands					= true
 end
 	-- Main Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.ViewModel					= "models/weapons/c_smg1.mdl"
-SWEP.WorldModel					= "models/weapons/w_smg1.mdl"
-SWEP.HoldType 					= "smg"
+SWEP.ViewModel					= "models/weapons/arccw_go/v_mach_m249para.mdl"
+SWEP.WorldModel					= "models/weapons/arccw_go/v_mach_m249para.mdl"
+SWEP.WorldModel_UseCustomPosition = true -- Should the gun use custom position? This can be used to fix guns that are in the crotch
+SWEP.WorldModel_CustomPositionAngle = Vector(0, 0, 180)
+SWEP.WorldModel_CustomPositionOrigin = Vector(-8, -15, 13)
+SWEP.WorldModel_CustomPositionBone = "ValveBiped.Bip01_R_Hand" -- The bone it will use as the main point (Owner's bone)
+SWEP.WorldModel_Invisible = false -- Should the world model be invisible?
+SWEP.WorldModel_NoShadow = false -- Should the world model have a shadow?
+SWEP.HoldType 					= "ar2"
 SWEP.Spawnable					= true
 SWEP.AdminSpawnable				= false
 	-- NPC Settings ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -24,49 +30,32 @@ SWEP.NPC_HasSecondaryFire = false -- Can the weapon have a secondary fire?
 SWEP.NPC_SecondaryFireSound = {"weapons/ar2/ar2_altfire.wav"} -- The sound it plays when the secondary fire is used
 	-- Primary Fire ---------------------------------------------------------------------------------------------------------------------------------------------
 SWEP.Primary.Damage				= 0 -- Damage
-SWEP.Primary.ClipSize			= 30 -- Max amount of bullets per clip
+SWEP.Primary.ClipSize			= 100 -- Max amount of bullets per clip
 SWEP.Primary.Delay				= 0.09 -- Time until it can shoot again
 SWEP.Primary.Automatic			= true -- Is it automatic?
 SWEP.Primary.Ammo				= "SMG1" -- Ammo type
-SWEP.Primary.Sound				= {"vj_weapons/hl2_smg1/smg1_single1.wav","vj_weapons/hl2_smg1/smg1_single2.wav","vj_weapons/hl2_smg1/smg1_single3.wav"}
-SWEP.Primary.DistantSound		= {"Weapon_SMG1.NPC_Single"}
-SWEP.NPC_TimeUntilFireExtraTimers = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6} -- Extra timers, which will make the gun fire again! | The seconds are counted after the self.NPC_TimeUntilFire!
+SWEP.Primary.Sound				= {"arccw_go/m249/m249-1.wav"}
+SWEP.Primary.DistantSound		= {"arccw_go/m249/m249-1-distant.wav"}
+SWEP.NPC_TimeUntilFireExtraTimers = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0} -- Extra timers, which will make the gun fire again! | The seconds are counted after the self.NPC_TimeUntilFire!
 SWEP.PrimaryEffects_MuzzleAttachment = 1
 SWEP.PrimaryEffects_ShellAttachment = 2
 SWEP.PrimaryEffects_ShellType = "VJ_Weapon_PistolShell1"
 SWEP.NPC_NextPrimaryFire = 5 -- Next time it can use primary fire
 	-- ====== Secondary Fire Variables ====== --
-SWEP.Secondary.Automatic = false -- Is it automatic?
+SWEP.Secondary.Automatic = true -- Is it automatic?
 SWEP.Secondary.Ammo = nil -- Ammo type
 	-- Reload Settings ---------------------------------------------------------------------------------------------------------------------------------------------
-SWEP.HasReloadSound = true -- Does it have a reload sound? Remember even if this is set to false, the animation sound will still play!
+SWEP.HasReloadSound = false -- Does it have a reload sound? Remember even if this is set to false, the animation sound will still play!
 SWEP.ReloadSound = {"weapons/smg1/smg1_reload.wav"}
 SWEP.Primary.Tracer = 0
 SWEP.Primary.DisableBulletCode = true
 ---------------------------------------------------------------------------------------------------------------------------------------------
-function SWEP:CustomOnSecondaryAttack()
-	local owner = self:GetOwner()
-	owner:ViewPunch(Angle(-self.Primary.Recoil *3, 0, 0))
-	VJ_EmitSound(self, "weapons/ar2/ar2_altfire.wav", 85)
-
-	local proj = ents.Create(self.NPC_SecondaryFireEnt)
-	proj:SetPos(owner:GetShootPos())
-	proj:SetAngles(owner:GetAimVector():Angle())
-	proj:SetOwner(owner)
-	proj:Spawn()
-	proj:Activate()
-	local phys = proj:GetPhysicsObject()
-	if IsValid(phys) then
-		phys:Wake()
-		phys:SetVelocity(owner:GetAimVector() * 2000)
-	end
-	return true
-end
-
 function SWEP:CustomOnPrimaryAttack_BeforeShoot()
 	if CLIENT then return end
 	local bullet = ents.Create("obj_vj_horde_bullet")
-	bullet:SetPos(self:GetAttachment(self:LookupAttachment("muzzle")).Pos)
+	local src = self:GetAttachment(1).Pos
+	local dir = (self:GetOwner():GetEnemy():GetPos() - self:GetOwner():GetPos())
+	bullet:SetPos(src)
 	bullet:SetAngles(self:GetOwner():GetAngles())
 	bullet:SetOwner(self:GetOwner())
 	bullet:Activate()
@@ -75,7 +64,6 @@ function SWEP:CustomOnPrimaryAttack_BeforeShoot()
 	
 	local phy = bullet:GetPhysicsObject()
 	if phy:IsValid() then
-		local dir = (self:GetOwner():GetEnemy():GetPos() - self:GetOwner():GetPos())
 		dir:Normalize()
 		dir = dir + VectorRand() * 0.03
 		dir:Normalize()
