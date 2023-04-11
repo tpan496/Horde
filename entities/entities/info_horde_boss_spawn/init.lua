9089c7e95eec64f8d8e9260d4b06fcbc637f579b
kirillbrest123 <70514395+kirillbrest123@users.noreply.github.com>
2023-04-11 18:42:33 +0300

    Update init.lua

--- entities/entities/info_horde_boss_spawn/init.lua
+++ entities/entities/info_horde_boss_spawn/init.lua
@@ -16,12 +16,15 @@ function ENT:AcceptInput(name, activator, caller, arg)
 	name = string.lower(name)
 	if name == "enable" or name == "unhide" then
 		self.Disabled = false
+		HORDE:AddBossNode(self:GetPos())
 		return true
 	elseif name == "disable" or name == "hide" then
 		self.Disabled = true
+		HORDE:RemoveBossNode(self:GetPos())
 		return true
 	elseif name == "toggle" then
 		self.Disabled = not self.Disabled
+		if self.Disabled then HORDE:RemoveBossNode(self:GetPos()) else HORDE:AddBossNode(self:GetPos()) end
 		return true
 	end
 end
