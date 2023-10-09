f511198f5ccfa0b739044b05c0777891c4db8942
Redox <mail@redox.to>
2023-10-09 17:21:47 +0200

    Fix nil error

--- gamemodes/horde/gamemode/status/buff/sv_warden_aura.lua
+++ gamemodes/horde/gamemode/status/buff/sv_warden_aura.lua
@@ -69,7 +69,7 @@ function plymeta:Horde_GetEnableWardenAuraBuffBonus()
 end
 
 function plymeta:Horde_AddWardenAuraEffects(provider)
-    if not provider or not provider:Alive() then return end
+    if not provider or not IsValid( provider ) or not provider:Alive() then return end
     if HORDE:IsWatchTower(provider) then
         self.Horde_WardenAuraProvider = provider:GetNWEntity("HordeOwner")
     else
