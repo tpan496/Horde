cc66448db8077fc03a8c73178afeeada315b6bba
Redox <mail@redox.to>
2023-10-15 01:34:33 +0200

    Fix crash alert spam

--- gamemodes/horde/entities/entities/horde_sticky_bomb.lua
+++ gamemodes/horde/entities/entities/horde_sticky_bomb.lua
@@ -4,19 +4,19 @@ ENT.Base = "base_anim"
 ENT.Spawnable = false
 
 function ENT:Draw()
-    self.Entity:DrawModel()
+    self:DrawModel()
 end
 
 function ENT:Initialize()
 if SERVER then
-    self.Entity:SetModel("models/horde/stickybomb/stickybomb.mdl")
-    self.Entity:SetMoveType( MOVETYPE_VPHYSICS )
-    self.Entity:SetSolid( SOLID_VPHYSICS )
-    self.Entity:PhysicsInit( SOLID_VPHYSICS )
-    self.Entity:SetCollisionGroup( COLLISION_GROUP_PROJECTILE )
-    self.Entity:DrawShadow( false )
+    self:SetModel("models/horde/stickybomb/stickybomb.mdl")
+    self:SetMoveType( MOVETYPE_VPHYSICS )
+    self:SetSolid( SOLID_VPHYSICS )
+    self:PhysicsInit( SOLID_VPHYSICS )
+    self:SetCollisionGroup( COLLISION_GROUP_PROJECTILE )
+    self:DrawShadow( false )
     local trail = ents.Create( "info_particle_system" )
-    trail:SetParent( self.Entity )
+    trail:SetParent( self )
     trail:SetOwner( self.Owner )
     trail:SetPos( self:GetPos() )
     trail:SetAngles( self:GetAngles() )
@@ -31,32 +31,39 @@ self.ExplodeTimer = CurTime() + 2.3
 end
 
 function ENT:Think()
-if self.Activation == 0 and self.ActivationTimer <= CurTime() then
-    if SERVER then
-        local pulse = ents.Create( "info_particle_system" )
-        pulse:SetKeyValue( "effect_name", "stickybomb_pulse_red" )
-        pulse:SetParent( self.Entity )
-        pulse:SetOwner( self.Owner )
-        pulse:SetPos( self:GetPos() )
-        pulse:SetAngles( self:GetAngles() )
-        pulse:Spawn()
-        pulse:Activate()
-        pulse:Fire( "start", "", 0 )
-        self.Pulse = pulse
+    if self.Activation == 0 and self.ActivationTimer <= CurTime() then
+        if SERVER then
+            local pulse = ents.Create( "info_particle_system" )
+            pulse:SetKeyValue( "effect_name", "stickybomb_pulse_red" )
+            pulse:SetParent( self )
+            pulse:SetOwner( self.Owner )
+            pulse:SetPos( self:GetPos() )
+            pulse:SetAngles( self:GetAngles() )
+            pulse:Spawn()
+            pulse:Activate()
+            pulse:Fire( "start", "", 0 )
+            self.Pulse = pulse
+        end
+        self.Activation = 1
     end
-    self.Activation = 1
-end
-if SERVER then
-    self.Trail:SetPos( self:GetPos() )
-end
+    if SERVER then
+        self.Trail:SetPos( self:GetPos() )
+    end
 end
 
 function ENT:PhysicsCollide(coldata, collider)
     if SERVER then
-        self.Entity:SetMoveType( MOVETYPE_NONE )
-        self.Entity:SetSolid( SOLID_NONE )
-        self.Entity:PhysicsInit( SOLID_NONE )
-        self.Entity:SetCollisionGroup( COLLISION_GROUP_NONE )
+        self:SetMoveType( MOVETYPE_NONE )
+        self:SetSolid( SOLID_NONE )
+        self:PhysicsInit( SOLID_NONE )
+
+        -- Fixes horde_sticky_bomb[362]: Changing collision rules within a callback is likely to cause crashes! spam
+        timer.Simple( 0, function()
+            if IsValid( self ) then
+                self:SetCollisionGroup( COLLISION_GROUP_NONE )
+            end
+        end )
+
         if coldata.HitEntity and coldata.HitEntity:IsValid() and coldata.HitEntity:IsNPC() then
             self:SetParent(coldata.HitEntity, 0)
         end
