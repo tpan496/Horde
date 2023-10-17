3bbaa64eeee8af53da8a1cd1dfa2975e4a68106c
Jagnt <148128844+Jagnt@users.noreply.github.com>
2023-10-17 13:43:06 -0400

    Molotov regens (#5)
    Cremator can now regenerate molotovs at a rate 50% slower than demolition can regenerate grenades

--- gamemodes/horde/gamemode/perks/cremator/cremator_base.lua
+++ gamemodes/horde/gamemode/perks/cremator/cremator_base.lua
@@ -8,6 +8,7 @@ Attacks have {5} chance to Ignite enemies.
 
 Ignite base duration is {6} and deals damage over time based on hit damage.
 Fire damage has {7} Ignite chance.]]
+
 PERK.Params = {
     [1] = {percent = true, base = 0.25, level = 0.01, max = 0.5, classname = HORDE.Class_Cremator},
     [2] = {value = 0.25, percent = true},
@@ -22,12 +23,25 @@ PERK.Hooks = {}
 PERK.Hooks.Horde_OnSetPerk = function(ply, perk)
     if SERVER and perk == "cremator_base" then
         ply:Horde_SetApplyIgniteChance(ply:Horde_GetApplyIgniteChance() + 0.15)
+        timer.Create("Horde_CrematorBase" .. ply:SteamID(), 45, 0, function ()
+            if not ply:IsValid() or not ply:Alive() then return end
+            if HORDE.items["arccw_thr_horde_molotov"] then
+                if not ply:HasWeapon("arccw_thr_horde_molotov") then
+                    ply:Give("arccw_thr_horde_molotov", ply:GetAmmoCount("arccw_thr_horde_molotov") > 0)
+                end
+            else
+                if not ply:HasWeapon("arccw_horde_nade_molotov") then
+                    ply:Give("arccw_horde_nade_molotov", ply:GetAmmoCount("Grenade") > 0)
+                end
+            end
+        end)
     end
 end
 
 PERK.Hooks.Horde_OnUnsetPerk = function(ply, perk)
     if SERVER and perk == "cremator_base" then
         ply:Horde_SetApplyIgniteChance(ply:Horde_GetApplyIgniteChance() - 0.15)
+        timer.Remove("Horde_CrematorBase" .. ply:SteamID())
     end
 end
 
