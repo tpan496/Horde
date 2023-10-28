6c350c76fd5a146d11f5e7c5b365645762bf522b
Jagnt <148128844+Jagnt@users.noreply.github.com>
2023-10-28 18:38:33 -0400

    Added halos for medic (#31)
    * Added halos for medic
    
    fixed number of halos made
    
    * Removed leftover print
    
    copy pasted from vs code and forgot to remove that lol
    
    * Added caching for colours
    
    * localized user health as a variable

--- gamemodes/horde/gamemode/perks/medic/medic_base.lua
+++ gamemodes/horde/gamemode/perks/medic/medic_base.lua
@@ -14,15 +14,42 @@ PERK.Params = {
 }
 
 PERK.Hooks = {}
+
+local healthColorCache = {}
+
+local function getHealthColor( health )
+    local cached = healthColorCache[health]
+    if cached then return cached end
+
+    local hue = math.Clamp(( health / 100 ) * 120, 0, 175)
+    local color = HSVToColor( hue, 1, 1 )
+    healthColorCache[health] = color
+    return color
+end
+
 PERK.Hooks.Horde_OnSetPerk = function(ply, perk)
     if SERVER and perk == "medic_base" then
         ply:Horde_SetHealthRegenPercentage(0.01)
     end
+    if perk == "medic_base" then
+        hook.Add("PreDrawHalos", "MedicHealthOutlines", function()
+            for _, user in ipairs(player.GetAll()) do
+                local HealthPoints = user:Health()
+                if HealthPoints > 0 and HealthPoints < user:GetMaxHealth() then
+                    halo.Add({user}, getHealthColor(HealthPoints), 2, 2, 1, true, false)
+                end
+            end
+        end)
+    end
 end
 
 PERK.Hooks.Horde_OnUnsetPerk = function(ply, perk)
-    if SERVER and perk == "medic_base" then
-        ply:Horde_SetHealthRegenPercentage(0)
+    if perk == "medic_base" then
+        if SERVER then
+            ply:Horde_SetHealthRegenPercentage(0)
+        else
+            hook.Remove("PreDrawHalos", "MedicHealthOutlines")
+        end
     end
 end
 
