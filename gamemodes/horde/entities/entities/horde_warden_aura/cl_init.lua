e90a07b412348258b0c3d57c1bb7e680977ebad6
MrSmig <148366458+MrSmig@users.noreply.github.com>
2023-11-16 22:59:46 +0000

    Add client convar to alter the warden ring's opacity (#49)

--- gamemodes/horde/entities/entities/horde_warden_aura/cl_init.lua
+++ gamemodes/horde/entities/entities/horde_warden_aura/cl_init.lua
@@ -1,5 +1,21 @@
 include("shared.lua")
 
+local OpacityConvar = CreateClientConVar("horde_warden_aura_ring_opacity",
+	"100",
+	true,
+	false,
+	"Controls the Opacity of the Warden Aura Rings, the higher, the more opaque/visible, Set to 0 to disable Warden Aura rings from rendering",
+	0, 
+	255
+)
+
+local opacity = OpacityConvar:GetInt()
+local col = Color(0,255,100,opacity)
+cvars.AddChangeCallback("horde_warden_aura_ring_opacity", function( convar_name, oldValue, newValue ) 
+	opacity = tonumber(newValue)
+	col = Color(0,255,100,opacity)
+end)
+
 local auras = auras or {}
 
 function ENT:Initialize()
@@ -10,9 +26,8 @@ function ENT:OnRemove()
 	auras[self] = nil
 end
 
-local col = Color(0,255,100,100)
 hook.Add( "PreDrawRings", "DrawWardenRings", function()
-	if next( auras ) == nil then return end
+	if next( auras ) == nil or opacity == 0 then return end
 	local set = {}
 	for aura in pairs(auras) do
 		table.insert(set,{aura, aura:GetRadius() or 160, 5, 30})
