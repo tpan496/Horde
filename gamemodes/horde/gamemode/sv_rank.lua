ee2e3ad0c39309fb288871d815b9e34f9ede9633
Redox <mail@redox.to>
2023-11-22 20:38:44 +0100

    Attribute skull token properly

--- gamemodes/horde/gamemode/sv_rank.lua
+++ gamemodes/horde/gamemode/sv_rank.lua
@@ -149,7 +149,7 @@ if GetConVar("horde_enable_sandbox"):GetInt() == 0 and GetConVar("horde_enable_r
 		if HORDE.difficulty >= 5 and HORDE.current_wave >= 4 then
 			ExpMulti = 2
 		end
-			
+
 		if HORDE.current_wave <= 0 or GetConVar("sv_cheats"):GetInt() == 1 then return end
 		if killer:IsPlayer() and killer:IsValid() and killer:Horde_GetClass() then
 			local class_name = killer:Horde_GetCurrentSubclass()
@@ -158,7 +158,7 @@ if GetConVar("horde_enable_sandbox"):GetInt() == 0 and GetConVar("horde_enable_r
 				killer:Horde_SetExp(class_name, killer:Horde_GetExp(class_name) + (2*ExpMulti) )
 				local p = math.random()
 				if p < 0.01 or (p < 0.1 and killer:Horde_GetGadget() == "gadget_corporate_mindset") then
-                    ent:Horde_AddSkullTokens(1)
+                    killer:Horde_AddSkullTokens(1)
 				end
 			else
 				killer:Horde_SetExp(class_name, killer:Horde_GetExp(class_name) + (1*ExpMulti) )
