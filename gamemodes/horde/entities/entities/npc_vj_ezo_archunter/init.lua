9f523e0fd8c2532d57ba6158b55ea51bafbb9cef
TheAmericanCandyVan <125820322+TheAmericanCandyVan@users.noreply.github.com>
2023-12-17 13:10:03 -0500

    removed line

--- gamemodes/horde/entities/entities/npc_vj_ezo_archunter/init.lua
+++ gamemodes/horde/entities/entities/npc_vj_ezo_archunter/init.lua
@@ -376,8 +376,6 @@ end
 
 function ENT:RangeAttackCode_GetShootPos(projectile)
 	local startPos = projectile:GetPos()
---	ParticleEffect("", startPos + projectile:GetForward() * 30, self:GetForward():Angle(), projectile)
-
 	return self:CalculateProjectile("Line", startPos, self:GetEnemy():GetPos() + self:GetEnemy():OBBCenter(), 2000)
 end
 
