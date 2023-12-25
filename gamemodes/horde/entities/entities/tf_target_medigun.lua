992c750c64b0d77931ded63e7824e41e04ee48ed
Redox <mail@redox.to>
2023-12-25 19:05:54 +0100

    Style

--- gamemodes/horde/entities/entities/tf_target_medigun.lua
+++ gamemodes/horde/entities/entities/tf_target_medigun.lua
@@ -7,13 +7,13 @@ function ENT:Draw()
 end
 
 function ENT:Initialize()
-if SERVER then
-self.Entity:SetModel( "models/hunter/plates/plate.mdl" )
-self.Entity:SetMoveType( MOVETYPE_NONE )
-self.Entity:SetSolid( SOLID_NONE )
-self.Entity:PhysicsInit( SOLID_NONE )
-self.Entity:SetCollisionGroup( COLLISION_GROUP_NONE )
-self.Entity:DrawShadow( false )
-self.Entity:SetName( self.Entity:GetName().."_"..self.Entity:EntIndex() )
-end
+    if SERVER then
+        self:SetModel( "models/hunter/plates/plate.mdl" )
+        self:SetMoveType( MOVETYPE_NONE )
+        self:SetSolid( SOLID_NONE )
+        self:PhysicsInit( SOLID_NONE )
+        self:SetCollisionGroup( COLLISION_GROUP_NONE )
+        self:DrawShadow( false )
+        self:SetName( self:GetName() .. "_" .. self:EntIndex() )
+    end
 end
